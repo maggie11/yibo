2c091f70c15de48b31c678cbaec2f464913a75e6
maggie <>
2021-01-31 16:08:03 +0800

    save

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -28,7 +28,7 @@ let fruitQTan = false;
 let fruitSlowDown = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'http://ggddcp.test.upcdn.net/vic.png';
+let adLink = 'https://maggie11.github.io/yibo/res/vic.png';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
 document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
